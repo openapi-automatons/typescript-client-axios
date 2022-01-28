d7c83ba898ec60e789144532de438cf6e2d370a9
tanmen <yt.prog@gmail.com>
2022-01-28 16:10:22 +0900

    chore: fix test

--- src/__tests__/generates/minimum.spec.ts
+++ src/__tests__/generates/minimum.spec.ts
@@ -1,7 +1,6 @@
 import {Openapi} from '@automatons/tools';
 import {remove} from "fs-extra";
-import {eslint} from "../../../test/utils/eslint";
-import {tsc} from "../../../test/utils/tsc";
+import {expectFormat} from "../../../test/expects/expectFormat";
 import {generate} from "../../generator";
 import paths from "../../paths";
 
@@ -10,8 +9,7 @@ jest.setTimeout(50000);
 it('should generate minimum', async () => {
   await generate(minimumOpenapi, {path: '', openapiPath: '', outDir: paths.tmp});
 
-  expect(async () => await tsc()).not.toThrow();
-  expect(async () => await eslint()).not.toThrow();
+  await expectFormat();
 });
 
 beforeEach(async () => {
