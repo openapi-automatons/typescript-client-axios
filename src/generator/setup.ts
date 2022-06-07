ff0e23ee9f2941c98fe9dfd183c9cdbd7e27bed5
tanmen <yt.prog@gmail.com>
2022-06-07 22:14:36 +0900

    fix: every function

--- src/generator/setup.ts
+++ src/generator/setup.ts
@@ -3,14 +3,14 @@ import HB from "handlebars";
 import {register} from "./register";
 
 HB.registerHelper('every', function <T extends Object>(value: T[], key: keyof T, text?: T[keyof T]) {
-  if (!text) {
+  if (text === undefined) {
     return value.every(item => (item[key] == undefined ? false : item[key]))
   }
   return value.every(item => (item[key] == undefined ? false : item[key]) === text)
 });
 
 HB.registerHelper('any', function <T extends Object>(value: T[], key: keyof T, text?: T[keyof T]) {
-  if (!text) {
+  if (text === undefined) {
     return value.some(item => (item[key] == undefined ? false : item[key]))
   }
   return value.some(item => (item[key] == undefined ? false : item[key]) === text)
